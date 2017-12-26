8d6a8bee1286ef29afd6b13e321e5943c76edf32
liuyuancheng <tenstone@foxmail.com>
2017-12-26 14:09:22 +0800

    1. 删除多余的static

--- examples/snowboy/demo.py
+++ examples/snowboy/demo.py
@@ -4,13 +4,16 @@ import signal
 
 interrupted = False
 
+
 def signal_handler(signal, frame):
     global interrupted
     interrupted = True
 
+
 def interrupt_callback():
     global interrupted
     return interrupted
+
 
 if len(sys.argv) == 1:
     print("Error: need to specify model name")
@@ -19,12 +22,14 @@ if len(sys.argv) == 1:
 
 model = sys.argv[1]
 
+# capture SIGINT signal, e.g., Ctrl+C
 signal.signal(signal.SIGINT, signal_handler)
 
 detector = snowboydecoder.HotwordDetector(model, sensitivity=0.5)
 print('Listening... Press Ctrl+C to exit')
 
-detector.start(detected_callback=snowboydecoder.ding_callback,
+# main loop
+detector.start(detected_callback=snowboydecoder.play_audio_file,
                interrupt_check=interrupt_callback,
                sleep_time=0.03)
 
