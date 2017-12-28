47b3e078d47b6efc63340dac7e03c51c8e72357d
liuyuancheng <tenstone@foxmail.com>
2017-12-28 12:57:19 +0800

    update

---
+++ src/plugins/News.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8-*-
+# 闲聊插件
+import sys
+
+# Standard module stuff
+WORDS = [u"新闻", u"今日头条"]
+SLUG = "chatting"
+
+
+def handle(text, mic, profile):
+    """
+    Responds to user-input, typically speech text
+
+    Arguments:
+        text -- user-input, typically transcribed speech
+        mic -- used to interact with the user (for both input and output)
+        profile -- contains information related to the user (e.g., phone
+                   number)
+        wxbot -- wechat bot instance
+    """
+    if not any(word in text for word in [u"结束", u"停止", u"退出", u"不聊了"]):
+        mic.say(u"进入闲聊模式，现在跟我说说话吧", cache=True)
+        mic.chatting_mode = True
+        mic.skip_passive = True
+    else:
+        mic.say(u"退出闲聊模式", cache=True)
+        mic.skip_passive = False
+        mic.chatting_mode = False
+
+
+def isValid(text):
+    """
+        Returns True if the input is related to weather.
+
+        Arguments:
+        text -- user-input, typically transcribed speech
+    """
+    return any(word in text for word in [u"闲聊", u"聊天", u"不聊了"])
