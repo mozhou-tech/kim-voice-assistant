b2b530983f60d1d206425d64983a73ab82eedf70
liuyuancheng <tenstone@foxmail.com>
2018-01-22 18:28:39 +0800

    处理IOT的问题

--- utils/aliyun_iotx/iot_server.py
+++ utils/aliyun_iotx/iot_server.py
@@ -20,13 +20,13 @@ class IotServer:
         发送设备期望状态
         :return:
         """
-        print(payload)
+        self._logger.info('通过服务器端API上报desired:'+payload)
         self.clt = client.AcsClient(self.accessKeyId, self.accessKeySecret, 'cn-shanghai')
         self.request = UpdateDeviceShadowRequest.UpdateDeviceShadowRequest()
         self.request.set_accept_format('json')  # 设置返回数据格式，默认为XML
         self.request.set_ProductKey(device.product_key)
-        self.request.set_DeviceName(device.device_name)  #消息发送到的Topic全名
-        self.request.set_ShadowMessage(base64.b64encode(payload.encode('utf8')))  #hello world Base64 String
+        self.request.set_DeviceName(device.device_name)  # 消息发送到的Topic全名
+        self.request.set_ShadowMessage(payload.encode('utf8'))  # hello world Base64 String
         result_json = json.loads(self.clt.do_action_with_exception(self.request).decode('utf8'))
         if result_json['Success']:
             self._logger.info('device desired send successed %s.', result_json['RequestId'])
